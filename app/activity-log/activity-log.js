7803632eee1472cd596f469bbc6e1c53a995c582
walter <waltermblair@gmail.com>
2018-04-02 22:44:57 -0400

    working insert

--- app/activity-log/activity-log.js
+++ app/activity-log/activity-log.js
@@ -3,12 +3,26 @@
 angular.module('myApp.activityLog', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/activity-log', {
-    templateUrl: 'activity-log/activity-log.html',
-    controller: 'ActivityLogCtrl'
-  });
+    $routeProvider.when('/activity-log', {
+        templateUrl: 'activity-log/activity-log.html',
+        controller: 'ActivityLogCtrl'
+    });
 }])
 
-.controller('ActivityLogCtrl', [function() {
-
+.controller('ActivityLogCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.postData = function() {
+        var request = $http({
+            url: 'http://localhost/angular-mysql/server.php',
+            method: 'POST',
+            data: {
+                description: $scope.description,
+                time: new Date(),
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function(response) {
+            console.log(response.data)
+        });
+    }
 }]);
